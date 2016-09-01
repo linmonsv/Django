b264292a938eb580bd1b404b0b3bd8d0065ebbaa
linmonsv <qin2@qq.com>
2016-09-01 15:58:09 +0800

    sayHello

--- SimpleBlog/SimpleBlog/settings.py
+++ SimpleBlog/SimpleBlog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^um07^3_qbj29pl$$pq+f9!)&q*=auk%(m#$5=5t@u0wgancv7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- SimpleBlog/SimpleBlog/urls.py
+++ SimpleBlog/SimpleBlog/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from sayhello.views import *
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^sayhello/',sayHello)
 ]
